0f201cb2da2890f40351f94a4f7ad686374e1e60
Marv934 <67321320+Marv934@users.noreply.github.com>
2022-01-25 07:31:27 +0100

    Mokup-Test

--- Assets/Scripts/GenerativeSoundEngine/GSE_AI_Test.cs
+++ Assets/Scripts/GenerativeSoundEngine/GSE_AI_Test.cs
@@ -22,37 +22,43 @@ namespace GenerativeSoundEngine
         {
             TimeSpan timePassed = nowDate - startDate;
 
-            if ((lastTimePassed.TotalSeconds < 2) && (2 <= timePassed.TotalSeconds))
+            if ((lastTimePassed.TotalSeconds < 3) && (3 <= timePassed.TotalSeconds))
             {
                 stateEngine = true;
-            } else if ((lastTimePassed.TotalSeconds < 3) && (3 <= timePassed.TotalSeconds))
+            } else if ((lastTimePassed.TotalSeconds < 7) && (7 <= timePassed.TotalSeconds))
             {
                 stateBlinker = 0.0f;
-            } else if ((lastTimePassed.TotalMilliseconds < 6600) && (6000 <= timePassed.TotalMilliseconds))
+            } else if ((lastTimePassed.TotalMilliseconds < 11600) && (11000 <= timePassed.TotalMilliseconds))
             {
                 stateSpeed = 5.0f;
                 stateThrottle = 0.33f;
-                stateSteering = (float)(timePassed.TotalMilliseconds - 6000) * -0.05f;
-            } else if ((lastTimePassed.TotalMilliseconds < 8300) && (6600 <= timePassed.TotalMilliseconds))
+                stateSteering = (float)(timePassed.TotalMilliseconds - 11000) * -0.05f;
+            } else if ((lastTimePassed.TotalMilliseconds < 13300) && (11600 <= timePassed.TotalMilliseconds))
             {
                 stateSteering = -30.0f;
-            } else if ((lastTimePassed.TotalMilliseconds < 9300) && (8300 <= timePassed.TotalMilliseconds))
+            } else if ((lastTimePassed.TotalMilliseconds < 14300) && (13300 <= timePassed.TotalMilliseconds))
             {
-                stateSteering = (float)(timePassed.TotalMilliseconds - 9300) * 0.03f;
-            } else if ((lastTimePassed.TotalMilliseconds < 9400) && (9300 <= timePassed.TotalMilliseconds))
-            {
-                stateSteering = 0.0f;
-            } else if ((lastTimePassed.TotalMilliseconds < 10390) && (9400 <= timePassed.TotalMilliseconds))
-            {
-                stateSteering = (float)(timePassed.TotalMilliseconds - 9400) * 0.03f;
-            } else if ((lastTimePassed.TotalMilliseconds < 11380) && (10390 <= timePassed.TotalMilliseconds))
-            {
-                stateSteering = (float)(timePassed.TotalMilliseconds - 11380) * -0.03f;
-            } else if ((lastTimePassed.TotalSeconds < 12) && (11380 <= timePassed.TotalMilliseconds))
+                stateSteering = (float)(timePassed.TotalMilliseconds - 14300) * 0.03f;
+            } else if ((lastTimePassed.TotalMilliseconds < 14400) && (14300 <= timePassed.TotalMilliseconds))
             {
                 stateSteering = 0.0f;
-                stateSpeed = 50.0f;
+            } else if ((lastTimePassed.TotalMilliseconds < 15389) && (14400 <= timePassed.TotalMilliseconds))
+            {
+                stateSteering = (float)(timePassed.TotalMilliseconds - 14400) * 0.03f;
+            } else if ((lastTimePassed.TotalMilliseconds < 16378) && (15389 <= timePassed.TotalMilliseconds))
+            {
+                stateSteering = (float)(timePassed.TotalMilliseconds - 16378) * -0.03f;
+            } else if ((lastTimePassed.TotalSeconds < 17) && (16378 <= timePassed.TotalMilliseconds))
+            {
+                stateBlinker = 0.5f;
+                stateSteering = 0.0f;
+                stateSpeed = 30.0f;
                 stateThrottle = 0.66f;
+            } else if ((lastTimePassed.TotalSeconds < 30) && (30 <= timePassed.TotalSeconds))
+            {
+                stateSpeed = 0.0f;
+                stateThrottle = 0.0f;
+                stateBreaking = 0.8f;
             }
 
 
