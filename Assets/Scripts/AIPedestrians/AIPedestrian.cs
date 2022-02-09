8c189cd0cccaa6e3d00888b8f8c13915b430132b
Marv934 <67321320+Marv934@users.noreply.github.com>
2022-02-09 11:58:01 +0100

    added Collision (not working)

--- Assets/Scripts/AIPedestrians/AIPedestrian.cs
+++ Assets/Scripts/AIPedestrians/AIPedestrian.cs
@@ -5,11 +5,15 @@ public class AIPedestrian : MonoBehaviour
 {
     WaypointProgressTracker _tracker;
     float _startYPosition;
+    WaypointCircuit _circuit;
+
+    bool triggered = false;
 
     public void Init(WaypointCircuit circuit)
     {
         enabled = true;
         _tracker = GetComponent<WaypointProgressTracker>();
+        _circuit = circuit;
         _tracker.enabled = true;
         _tracker.Init(circuit);
         _startYPosition = transform.position.y;
@@ -17,12 +21,24 @@ public class AIPedestrian : MonoBehaviour
 
     private void Update()
     {
-        var steer = Quaternion.LookRotation(_tracker.target.position - transform.position, Vector3.up).eulerAngles;
-        var rot = transform.eulerAngles;
-        rot.y = steer.y;
-        transform.eulerAngles = rot;
-        var pos = transform.position;
-        pos.y = _startYPosition;
-        transform.position = pos;
+        //if (triggered)
+        //{ 
+            var steer = Quaternion.LookRotation(_tracker.target.position - transform.position, Vector3.up).eulerAngles;
+            var rot = transform.eulerAngles;
+            rot.y = steer.y;
+            transform.eulerAngles = rot;
+            var pos = transform.position;
+            pos.y = _startYPosition;
+            transform.position = pos;
+        //}
     }
+
+//    public void Trigger()
+//    {
+//        triggered = true;
+//
+//        _tracker.enabled = true;
+//        _tracker.Init(_circuit);
+//        _startYPosition = transform.position.y;
+//    }
 }
---
+++ Assets/Scripts/GenerativeSoundEngine/GSE_PedestrianTriggerStart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+namespace GenerativeSoundEngine
+{
+    public class GSE_PedestrianTriggerStart : MonoBehaviour
+    {
+        bool destroy = true;
+
+        WaypointProgressTracker _AIPedestrian;
+
+        GSE_Collision Collision;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _AIPedestrian = GetComponentInParent<WaypointProgressTracker>();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            Collision = other.gameObject.GetComponent<GSE_Collision>();
+
+            if ( Collision != null)
+            {
+                //_AIPedestrian.Trigger();
+
+                if (destroy)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
